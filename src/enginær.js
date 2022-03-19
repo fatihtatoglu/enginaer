2d0f03a6be19f766fba604827d8010976cdba597
Fatih Tatoğlu <fatih@tatoglu.net>
2022-03-19 19:57:15 +0300

    add pages tag into templateData

--- src/enginær.js
+++ src/enginær.js
@@ -14,6 +14,7 @@ class Enginær {
     #options;
     #pages;
     #enrichers;
+    #templatePages;
 
     constructor() {
         this.#enrichers = {};
@@ -88,6 +89,7 @@ class Enginær {
         var menu = this.#options.get("menu") || {};
 
         this.#pages = new Map();
+        this.#templatePages = [];
         return through.obj(function (file, encoding, cb) {
             if (!that.#checkPageFileSanity(file, cb)) {
                 return;
@@ -150,6 +152,25 @@ class Enginær {
                 "content": htmlContent
             });
 
+            var templatePage = {
+                "name": pageName
+            };
+
+            metadata.forEach((v, k) => {
+
+                if (k === "order") {
+                    v = parseInt(v);
+                }
+
+                if (k === "published") {
+                    v = v === "true" ? true : false;
+                }
+
+                templatePage[k] = v;
+            });
+
+            that.#templatePages.push(templatePage);
+
             that.#options.set("menu", menu);
 
             cb(null, file);
@@ -209,6 +230,8 @@ class Enginær {
             metadata.forEach((v, k) => {
                 templateData[k] = v;
             });
+
+            templateData["pages"] = that.#templatePages;
 
             var permalink = templateData["permalink"];
             var output = mustache.render(template, templateData, templates);
@@ -315,7 +338,7 @@ class Enginær {
             }
 
             var value = kvp[1];
-            
+
             metadata.set(key, value);
         });
 
