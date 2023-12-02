a63087595c8f6de84d975af9019c92f2c2f4a038
Fatih Tatoğlu <fatihtatoglu@gmail.com>
2023-12-03 01:09:06 +0300

    add work plan

--- demo-site/gulpfile.js
+++ demo-site/gulpfile.js
@@ -2,22 +2,33 @@ const { series, parallel, src, dest } = require("gulp");
 const clean = require("gulp-clean");
 const Enginaer = require("enginaer");
 
-var enginær = new Enginaer(config);
+var enginær = new Enginaer();
 
-// load config
-// validate config
+// load config --> search 'enginear.json' file in the root folder.
+// validate config --> check only required configs, such as site.language & site.url
 
 // load resources
-// validate resources
-//// missing language ?
+// - assets --> just for copy
+// - content
+// - template
+// - template plugins
+// - content visitors
 
 // process resources
+// - content
+// - template
 
-// prebuild static content
+// prebuild static content -> process md operations
+// - markdown visitors
 
-// build static content
+// build static content -> convert md to partial html
+// - execute template plugins
+// - execute content visitors
+// - merge with templates
 
 // postbuild static content
+// - sitemap data
+// - rss feed data
 
 // prepare or clean destination
 function cleanAll() {
@@ -26,6 +37,10 @@ function cleanAll() {
 }
 
 // prepare output files
+// - css
+// - js
+// - img
+// - content output
 
 // save output files
 function save() {
